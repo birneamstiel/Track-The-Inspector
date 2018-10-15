afeefd8192e4c26fcf50382859969ead9e2f1c9f
birneamstiel <luckifri@yahoo.de>
2018-10-15 14:06:29 +0200

    Use env variables.

---
+++ bot/src/bot.py
@@ -0,0 +1,21 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def message(bot, update):
+	print(update.message.text)
+
+
+
+updater = Updater(TELEGRAM_API_KEY)
+
+# updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message))
+
+updater.start_polling()
+updater.idle()
