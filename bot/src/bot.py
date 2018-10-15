bd34c3e1750aa6439da9516838e38f1341d222aa
birneamstiel <luckifri@yahoo.de>
2018-10-15 15:20:07 +0200

    Exit program if api key is not set in .env file.

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -1,27 +1,22 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
-
+if not TELEGRAM_API_KEY:
+	sys.exit("Error: Telegram bot api key is not set. Create .env file and set TELEGRAM_API_KEY.")
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
-
 def message(bot, update):
 	print(update.message.text)
 
-
-
 updater = Updater(TELEGRAM_API_KEY)
 
-# updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, message))
 
 updater.start_polling()
