aef411b6911110e9a01683cd93ce6a7071aaec61
birneamstiel <luckifri@yahoo.de>
2018-10-15 19:37:48 +0200

    Use BVG api to find out name and location of raw input.

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
 import sys
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,7 +14,23 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 def message(bot, update):
-	print(update.message.text)
+	# print(update.message.text)
+	getTrainStation(update.message.text)
+
+def getTrainStation(raw_message):
+	query = {'query': raw_message}
+	r = requests.get('https://1.bvg.transport.rest/locations', params=query)
+	print(r.url)
+	# print(r.content)
+	data = r.json()
+	
+	i = 0
+	while data[i]['type'] != "stop":
+		i = i + 1
+	stop = data[i]
+	print(stop) 
+	# print(data[i]['name'])
+
 
 updater = Updater(TELEGRAM_API_KEY)
 
