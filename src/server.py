584f811fa8fa8645ac35146bd5e714a793a11f8d
birneamstiel <luckifri@yahoo.de>
2018-10-17 15:22:07 +0200

    Transfer message encoding logic into server to handle telegram webhooks.

--- src/server.py
+++ src/server.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from tinydb import TinyDB, Query
 from dotenv import load_dotenv
 import os
 import time
+import requests
+from geojson import Feature, Point
 
 app = Flask(__name__)
 db = TinyDB('db.json')
 
 load_dotenv()
 MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
+TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
 if not MAPBOX_TOKEN:
 	sys.exit("Error: mapbox api token is not set. Create .env file and set MAPBOX_TOKEN.")
+if not TELEGRAM_API_KEY:
+	sys.exit("Error: Telegram bot api key is not set. Create .env file and set TELEGRAM_API_KEY.")
 
 
 @app.route('/')
@@ -26,7 +31,35 @@ def hello_world():
 	return render_template('map.html', inspectors=inspectors, mapboxToken=MAPBOX_TOKEN)
 	
 
-    
+@app.route("/{}".format(TELEGRAM_API_KEY), methods=["POST"])
+def process_update():
+	if request.method == "POST":
+		update = request.get_json()
+		if "message" in update:
+			print('message!')
+			getTrainStation(update['message']['text'])
+		return "ok!", 200
+
+def getTrainStation(raw_message):
+	query = {'query': raw_message}
+	r = requests.get('https://1.bvg.transport.rest/locations', params=query)
+	print(r.url)
+	# print(r.content)
+	data = r.json()
+	
+	i = 0
+	while data[i]['type'] != "stop":
+		i = i + 1
+	raw_station = data[i]
+	print(raw_station ) 
+	station_geometry = Point((raw_station['location']['longitude'], raw_station['location']['latitude']))
+	station = Feature(geometry=station_geometry, properties={'name': raw_station['name'], 'rawValue': raw_message})
+	print('inserting: ', station) 
+	
+	data = {}
+	data['geoJson'] = station
+	data['timestamp'] = time.time()
+	db.insert(data)
 
 def handle_inspector(inspector):
 	print(inspector['geoJson'])
